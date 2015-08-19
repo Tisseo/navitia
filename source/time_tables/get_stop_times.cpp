9148a383234efa1485e49c16c5b6fa2294a755fc
Xavier RAFFIN <xavier.raffin@tisseo.fr>
2015-08-19 14:56:29 +0200

    Bugfix : routes_schedules with a given calendar had wrong datetime when route vehicule_journeys pass over midnight + utc_to_local_offset (ex : for GMT+2 : 1h / 2h / 3h stop_times in ed are 85400 / 0 / 3600 before correction routes_schedules returns : 20150818T020000 / 20150818T030000 / 20150819T010000 (last one on the next day) after correction routes_schedules returns : 20150818T010000 / 20150818T020000 / 20150818T030000

--- source/time_tables/get_stop_times.cpp
+++ source/time_tables/get_stop_times.cpp
@@ -183,11 +183,12 @@ get_all_stop_times(const type::JourneyPatternPoint* jpp,
                 }
 
                 //we need to convert this to local there since we do not have a precise date (just a period)
-                res.push_back({time + freq_vj->utc_to_local_offset, &st});
+				//adding utc_to_local_offset could overlap midnight -> we trunk to stay on the same date
+                res.push_back({(time + freq_vj->utc_to_local_offset)%DateTimeUtils::SECONDS_PER_DAY, &st});
             }
         } else {
             //same utc tranformation
-            res.push_back({st.departure_time + vj->utc_to_local_offset, &st});
+            res.push_back({(st.departure_time + vj->utc_to_local_offset)%DateTimeUtils::SECONDS_PER_DAY, &st});
         }
     }
 
